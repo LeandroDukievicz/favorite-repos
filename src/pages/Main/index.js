f3d454d23378d78654a18d889efc74956043b450
LeandroDukievicz <ldukie@icloud.com>
2024-07-26 16:56:14 -0300

    :lipstick: - Added style from page Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from 'react'
+import {FaGithub, FaPlus } from 'react-icons/fa'
 
-import { Title } from './styles'
+import { Title, Container, Form, SubmitButton } from './styles'
  
 export default function Main(){
     return(
-        <Title>Main</Title>
+        <Container>
+            <Title>
+                <FaGithub size={30}/> &nbsp;
+                <h1>
+                    Meus Repositórios :
+                </h1>                
+            </Title>
+
+            <Form onSubmit={()=> {}}>
+                <input type="text" placeholder="Adicionar Repositórios"/>
+
+                <SubmitButton>
+                    <FaPlus color="#fff" size={15} />
+                </SubmitButton>
+
+            </Form>
+        </Container>
     )
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,50 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
-    color: #000000;
+    color: #000;
+`;
 
+export const Container = styled.section`
+    max-width: 700px;
+    background-color: aliceblue;
+    border-radius: 10px;
+    padding: 30px;
+    margin: 80px auto;
+    box-shadow: 0 0 20px rgba(0,0,0, 0.2 );
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h1{
+        font-size:20px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`;
+
+export const Form = styled.form`
+    width: 650px;
+    color: #000;
+    display: flex;
+    justify-content: center ;
+    margin-top: 30px;
+
+    input{
+        flex:1;
+        padding:10px;
+        border: 2px inset #ddd;
+        border-end-start-radius: 5px;
+    }
+`;
+
+export const SubmitButton = styled.button`
+    background-color: #000;
+    color: #fff;
+    padding: 0 15px;
+    border-end-end-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
